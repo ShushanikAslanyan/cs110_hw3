90b9823418bb84d7da63cf5cc5ccca630d27f553
ShushanikAslanyan <sh.ledzeppelin@mail.ru>
2016-10-18 17:27:49 +0400

    Update server.js

--- server.js
+++ server.js
@@ -11,20 +11,20 @@ else if (req.url === '/goodbye') {
         res.end('Goodbye World!\n');
     }
 else if (req.url ==='/PinkFloyd') {
-      res.writeHead (200, {'Content-Type' : 'text/plain'});
-	  res.end ('I wll see you on the dark side of the moon\n');
+        res.writeHead (200, {'Content-Type' : 'text/plain'});
+	res.end ('I wll see you on the dark side of the moon\n');
 	}
 else if (req.url === '/cake') {
         res.writeHead (200, {'Content-Type' : 'text/plain'});
-		res.end ('Yummy!\n');
+	res.end ('Yummy!\n');
 	}
 else if (req.url === '/coffee') {
         res.writeHead (200, {'Content-Type' : 'text/plain'});
-		res.end ('Yay!\n');
+	res.end ('Yay!\n');
     }
 else if (req.url === '/TheBestCartoon') {
         res.writeHead (200, {'Content-Type' : 'text/plain'});
-		res.end ('Rick and Morty\n');
+	res.end ('Rick and Morty\n');
     }
 else {
         res.writeHead(404, {'Content-Type' : 'text/plain'});
